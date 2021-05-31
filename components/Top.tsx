544c02693e35f76aa9c50310c979e844720aec8c
Ivo Sequeros del Rey <ivosequeros@me.com>
2021-05-31 17:25:49 +0200

    Tweaked design

--- components/Top.tsx
+++ components/Top.tsx
@@ -2,7 +2,7 @@ import { GitHub, Linkedin } from "react-feather"
 
 const Top = () => {
   return (
-    <header className="flex text-sm justify-between">
+    <header className="flex text-sm justify-between flex-wrap">
       <div className="flex items-center flex-wrap">
         <div className="mb-4 sm:mb-0 w-full sm:w-auto"><img width="40px" src="/foto.png" className="mr-4 rounded-3xl" /></div>
         <div className="flex flex-wrap">
@@ -10,7 +10,7 @@ const Top = () => {
           <div className="md:ml-4 text-gray">Engineering, Design &#x26; Innovation</div>
         </div>
       </div>
-      <div className="flex text-gray pt-3 gap-5">
+      <div className="flex text-gray pt-3 gap-5 items-center">
         <a href="http://linkedin.com/in/ivo-s-393598131" target="_blank" className="hover:text-fg"><Linkedin size="16px" /></a>
         <a href="https://github.com/ivosequeros" target="_blank" className="hover:text-fg"><GitHub size="16px" className="mt-0.5" /></a>
         <a href="mailto:ivosequeros@me.com" className="hover:text-fg">Contact</a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,11 @@ const Home = () => {
       <div className="w-full flex-shrink">
         <Top />
         
-        <main className="mt-12 text-xl">
+        <main className="mt-10 md:mt-12 text-xl">
           <div>Hi, I’m Ivo. A full-stack engineer and designer from Barcelona, currently in London, UK.</div>
           <div className="text-base text-gray mt-2 md:mt-0">I love innovation, complex challenges and learning. Don’t hesitate to contact me for any challenge or proposal.</div>
 
-          <section className="mt-16">
+          <section className="mt-10 md:mt-16">
             <h2 className="">Open Projects</h2>
             <div className="flex mt-5">
               <a 
