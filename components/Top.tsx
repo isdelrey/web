d085fd1118a0d7d04ff81a245638044913e474e6
Ivo Sequeros del Rey <ivosequeros@me.com>
2021-06-01 13:41:58 +0200

    Updated github link

--- components/Top.tsx
+++ components/Top.tsx
@@ -12,7 +12,7 @@ const Top = () => {
       </div>
       <div className="flex text-gray pt-3 gap-5 items-center">
         <a href="http://linkedin.com/in/ivo-s-393598131" target="_blank" className="hover:text-fg"><Linkedin size="16px" /></a>
-        <a href="https://github.com/ivosequeros" target="_blank" className="hover:text-fg"><GitHub size="16px" className="mt-0.5" /></a>
+        <a href="https://github.com/isdelrey" target="_blank" className="hover:text-fg"><GitHub size="16px" className="mt-0.5" /></a>
         <a href="mailto:ivosequeros@me.com" className="hover:text-fg">Contact</a>
       </div>
     </header>
